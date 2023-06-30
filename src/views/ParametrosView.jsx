5a874d74cee5671dc50c7bf570dcf0aa9f2101cf
david diaz <david.diaz190799@gmail.com>
2023-06-29 22:45:29 -0500

    se instala libreria

--- src/views/ParametrosView.jsx
+++ src/views/ParametrosView.jsx
@@ -1,10 +1,181 @@
+import React,{useState} from "react";
+import { Grid } from "@mui/material";
+import Container from '@mui/material/Container';
+import Paper from '@mui/material/Paper';
+import Table from '@mui/material/Table';
+import TableBody from '@mui/material/TableBody';
+import TableCell from '@mui/material/TableCell';
+import TableContainer from '@mui/material/TableContainer';
+import TableHead from '@mui/material/TableHead';
+import TablePagination from '@mui/material/TablePagination';
+import IconButton from '@mui/material/IconButton';
+import TableRow from '@mui/material/TableRow';
+import SettingsIcon from '@mui/icons-material/Settings';
+import Button from '@mui/material/Button';
+import { Modal, ModalHeader, ModalBody, ModalFooter } from 'reactstrap';
 export default function ParametrosView(){
 
-
+    const [parametros,setParametros] = useState([{}]);
+    const [page, setPage] = useState(0);
+    const [rowsPerPage, setRowsPerPage] = useState(10);
+    const [modalCrear,setModalCrear]  = useState(false);
+  
+    const handleChangePage = (event, newPage) => {
+      setPage(newPage);
+    };
+  
+    const handleChangeRowsPerPage = (event) => {
+      setRowsPerPage(+event.target.value);
+      setPage(0);
+    };
     return(
 
         <>
-        <h1>parametros View</h1>
+            <Container maxWidth="md" style={{paddingTop:20}}>
+                <Grid container spacing={2}>
+                <Grid item xs={12} >
+                <Button variant="contained" onClick={()=>{setModalCrear(true)}}>Agregar Parametro</Button>
+                </Grid>
+                    <Grid item xs={12} >
+                    <Paper sx={{ width: '100%', overflow: 'hidden' }}>
+                        <TableContainer sx={{ maxHeight: 440 }}>
+                        <Table stickyHeader aria-label="sticky table">
+                                    <TableHead>
+                                        <TableRow>
+                                        <TableCell
+                                                key={"last"}
+                                                align={"left"}
+                                                style={{ minWidth: 200 }}
+                                                >
+                                                Nombre
+                                            </TableCell>
+                                            <TableCell
+                                                key={"Name"}
+                                                align={"left"}
+                                                style={{ minWidth: 200 }}
+                                                >
+                                                Marca
+                                            </TableCell>
+                                            <TableCell
+                                                key={"ip"}
+                                                align={"left"}
+                                                style={{ minWidth: 100 }}
+                                                >
+                                                Rango
+                                            </TableCell>
+                                            <TableCell
+                                                key={"modelo"}
+                                                align={"left"}
+                                                style={{ minWidth: 100 }}
+                                                >
+                                                Configuracion
+                                            </TableCell>
+                                           
+                                      
+                                        </TableRow>
+                                    </TableHead>
+                                    <TableBody>
+                                    {parametros.slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage)
+                                    .map((row,index) => {
+                                        return (
+                                            <TableRow key={index}>
+                                                 <TableCell align="left">
+                                                    {row.ultima_conexion}
+                                                </TableCell>
+                                                <TableCell align="left">
+                                                    {row.nombre}
+                                                </TableCell>
+                                                <TableCell align="left">
+                                                    {row.ip}
+                                                </TableCell>
+                                                <TableCell align="center">
+                             
+                                                        <IconButton color="rojo" aria-label="eliminar">
+                                                            <SettingsIcon />
+                                                        </IconButton>
+                                                       
+                                                </TableCell>
+
+                                            </TableRow>);
+                                        })}
+                                    </TableBody>
+                                 
+                                </Table>
+                            </TableContainer>
+                            <TablePagination
+                                        rowsPerPageOptions={[10, 25, 100]}
+                                        component="div"
+                                        count={parametros.length}
+                                        rowsPerPage={rowsPerPage}
+                                        page={page}
+                                        onPageChange={handleChangePage}
+                                        onRowsPerPageChange={handleChangeRowsPerPage}
+                                    />
+                        </Paper>
+                    </Grid>
+                </Grid>
+                
+                <Modal isOpen={modalCrear} >
+                    <ModalHeader>
+                        <div>
+                            <h1>
+                                Informacion
+                            </h1>
+                        </div>
+                    </ModalHeader>
+                    <ModalBody>
+                        <Grid container spacing={1}>
+                            <Grid item xs={6}>
+                                <div>
+                                    <strong><h5>Nombre</h5></strong>
+                                   
+                                </div>
+                            </Grid>
+                            <Grid item xs={6}>
+                                <div>
+                                    <strong><h5>Canton</h5></strong>
+                          
+                                </div>
+                            </Grid>
+                            <Grid item xs={6}>
+                                <div>
+                                    <strong><h5>Latitud</h5></strong>
+                              
+                                </div>
+                            </Grid>
+                            <Grid item xs={6}>
+                                <div>
+                                    <strong><h5>longitud</h5></strong>
+                    
+                                </div>
+                            </Grid>
+                            <Grid item xs={6}>
+                                <div>
+                                    <strong><h5>Ip</h5></strong>
+                   
+                                </div>
+                            </Grid>
+                            <Grid item xs={6}>
+
+                                <div>
+                                    <strong><h5>Mac</h5></strong>
+                  
+                                </div>
+                            </Grid>
+                        </Grid>
+
+
+                    </ModalBody>
+                    <ModalFooter >
+                        <Button variant="contained" color='anaranjado1'  sx={{ marginLeft: 1 }}>
+                            Aplicar
+                        </Button>
+                        <Button variant="contained" color='rojo'  sx={{ marginLeft: 1 }}>
+                            cancelar
+                        </Button>
+                    </ModalFooter>
+                </Modal>
+            </Container>
         </>
     )
 
